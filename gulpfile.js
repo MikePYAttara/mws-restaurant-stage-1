cf17013b4a5aa65ea0a34c6f44e96f1b2886fafb
Mike Perry Y Attara <mpyebattara@gmail.com>
2018-09-07 13:37:28 +0000

    chore: fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,49 @@
-const gulp = require('gulp');
-const minify = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('uglify');
-const autoprifixer = require('gulp-autoprefixer');
-const imagemin = require('imagemin');
+let gulp = require('gulp');
+let minify = require('gulp-clean-css');
+let sourcemaps = require('gulp-sourcemaps');
+let uglify = require('gulp-uglify');
+let autoprifixer = require('gulp-autoprefixer');
+let imagemin = require('imagemin');
 
 gulp.task('css', () => {
-    return gulp.src('./src/css/**/*.css')
+    return gulp.src('src/css/**/*.css')
     .pipe(sourcemaps.init())
-    .pipe(autoprifixer())
+    .pipe(autoprifixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(minify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('js', () => {
-    return gulp.src('./src/js/**/*.js')
+    return gulp.src('src/js/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('html', () => {
-	return gulp.src('./src/**/*.html')
-	gulp.pipe(gulp.dest('./dist'))
+	return gulp.src('src/**/*.html')
+	.pipe(gulp.dest('dist'))
 });
 
 gulp.task('images', () => {
-	return gulp.src("./src/img/**/*.{'jpg','jpeg','png','svg'}")
+	return gulp.src("src/img/**/*.{'jpg','jpeg','png','svg'}")
 	.pipe(imagemin())
-	.pipe(gulp.dest('/dist/img'))
+	.pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('manifest', () => {
-	return gulp.src('./manifest/*')
-	.pipe(gulp.dest('./dist/manifest'))
+	return gulp.src('manifest/*')
+	.pipe(gulp.dest('dist/manifest'))
 });
 
 gulp.task('sw', () => {
-	return gulp.src('./sw.js')
-	.pipe(gulp.dest('./dist'))
+	return gulp.src('sw.js')
+	.pipe(gulp.dest('dist'))
 });
 
 
