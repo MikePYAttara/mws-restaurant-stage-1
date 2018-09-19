0614cae09facb968ba897b33ba08b3e0d6d2d2f7
Mike Perry Y Attara <mpyebattara@gmail.com>
2018-09-19 14:47:14 +0000

    chore: add workbox

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const autoprifixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
+const workbox = require('workbox-build');
 
 gulp.task('css', () => {
     return gulp.src('src/css/**/*.css')
@@ -18,7 +19,7 @@ gulp.task('css', () => {
     .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('js', cb => {
+gulp.task('js', () => {
     gulp.src(['src/**/*.js'])
     .pipe(sourcemaps.init())
     .pipe(babel({
@@ -44,6 +45,21 @@ gulp.task('manifest', () => {
 	return gulp.src(['src/manifest/*'])
 	.pipe(gulp.dest('dist/manifest'))
 });
+
+gulp.task('service-worker', () => {
+    return workbox.injectManifest({
+      swSrc: 'src/sw.js',
+      swDest: 'build/sw.js',
+      globDirectory: 'build',
+      globPatterns: [
+        '**\/*.{js,css,html,png}',
+      ]
+    }).then(({count, size, warnings}) => {
+      // Optionally, log any warnings and details.
+      warnings.forEach(console.warn);
+      console.log(`${count} files will be precached, totaling ${size} bytes.`);
+    });
+  });
 
 gulp.task('watch', () => {
     gulp.watch(['src/*.html', 'src/**/*.js', 'src/css/*.css', 'src/reviews.webmanifest'], 'default')
